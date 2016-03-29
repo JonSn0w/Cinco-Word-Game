8383124eb00ebda8c4b74271841942f30b682b62
Brett Stevenson <therealbrettstevenson@gmail.com>
2016-03-28 19:35:02 -0700

    Updated Dictionary.java

--- Dictionary.java
+++ Dictionary.java
@@ -1,94 +1,88 @@
 // Cinco! Word Game
 // by Brett Stevenson
 // Dictionary.java
-/* 	This class reads words from a text file and stores them in an ArrayList, which it then 
-	modifies according to the user input. This class also provides a random guess as well as 
-	other supplementary information needed by the CincoGuesser class about the word list. 
-	Furthermore, since words with repeating characters aren't currently supported "secret words" 
-	it features a function to remove these invalid words.   */
+// Date Modified: 03/28/2016
+/* 	This class reads words from a text file and stores them in an ArrayList, which it then
+	modifies according to the user input. This class also provides a random guess, as well as
+	other supplementary information needed by the CincoGuesser class about the word list.
+	Furthermore, since words with repeating characters aren't currently supported 
+	"secret words" it features a function to remove these invalid words.   			       */
 // Status: Working/Tested.
 //
 
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.io.File; 
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.Random;
 
 class Dictionary {
 
-    private ArrayList <String> wordList = new ArrayList<String>();
-    final int WORDLENGTH = 5;
-    Random random = new Random();
+    private ArrayList <String> wordList = new ArrayList<>();
+    private final int WORDLENGTH = 5;
 
-    public Dictionary(String filename) {  // constructor: reads words from the supplied .txt file
-	try {
-	    Scanner scan = new Scanner (new File(filename));
-	    while(scan.hasNext())
-     		wordList.add(scan.next());
-	    scan.close();
-	} 
-	catch(FileNotFoundException e) {
-	    System.err.println(filename + ": File not found!");
-	}
+    public Dictionary(String filename) {  // constructor: reads words from the supplied word list
+        try {
+            Scanner scan = new Scanner (new File(filename));
+            while(scan.hasNext())
+                wordList.add(scan.next());
+            scan.close();
+        }
+        catch(FileNotFoundException e) {
+            System.err.println("The necessary file \'" + filename + "\' could not found!");
+        }
+        removeRepeatChars();
     }
-    
+
     public int getWordLength() {return WORDLENGTH;} // returns word length.
-    public int getWordCount() {return wordList.size();} // returns the remaining # of words in the list.
-    
-    public String getRandomWord() {
-	// returns a randomly selected guess from the remaining words.
-	int randomInt = random.nextInt(wordList.size());
-	String guess = wordList.get(randomInt);
-	return guess;
+    public int getWordCount() {return wordList.size();} // returns the remaining # of words.
+
+    public String getRandomWord() {  // returns a randomly selected word from the word list.
+        Random rndm = new Random();
+        int randomIndex = rndm.nextInt(wordList.size());
+        return wordList.get(randomIndex);
     }
-    
+
     public void removeWords(String guess, int matching, int inPlace) {
-	// removes words from the supplied list that don't fit the given criteria
-        String secondWord;
-	int commonMatch, commonPlace;
-	for(int i = wordList.size()-1; i >= 0; i--) {
-	    secondWord = wordList.get(i);
-	    commonMatch = countMatching(guess, secondWord);
-	    commonPlace = countInPlace(guess, secondWord);
-	    if (matching != commonMatch || inPlace != commonPlace) {
-		wordList.remove(i);
-	    }
-	}
-    }
-    
-    private int countMatching(String firstWord, String secondWord) {
-	// returns # of matching chars (helper of removeWords)
-	int commonMatch = 0;
-	char [] first = firstWord.toCharArray();
-	char [] second = secondWord.toCharArray();
-	for (int i = 0; i < WORDLENGTH; i++) 
-	    for (int count = WORDLENGTH-1; count >= 0; count--)
-		if (first[i] == second[count])
-		    commonMatch++;
-	return commonMatch;
-    }
-    
-    private int countInPlace(String firstWord, String secondWord) {
-	// returns # of in-place chars (helper of removeWords)
-	int commonPlace = 0;
-	char [] first = firstWord.toCharArray();
-	char [] second = secondWord.toCharArray();
-	for(int i = 0; i < WORDLENGTH; i++)
-	    if(first[i] == second[i])
-		commonPlace++;
-	return commonPlace;
+        // removes words from the list that don't fit the given criteria
+        for(int i = wordList.size()-1; i >= 0; i--) {
+            String listWord = wordList.get(i);
+            int matchCount = countMatching(guess, listWord);
+            int placeCount = countInPlace(guess, listWord);
+            if(matchCount != matching || placeCount != inPlace)
+                wordList.remove(i);
+        }
     }
 
-    public void repeatedChars() {
-	// removes all words with repeating characters from the supplied list
-	for(int i = wordList.size()-1; i >= 0; i--) {
-	    String word = wordList.get(i);
-	    if(countMatching(word, word) > 5)
-		wordList.remove(i); 	
-	}
+    private int countMatching(String guess, String listWords) {
+        // returns # of matching chars (helper of removeWords)
+        int matching = 0;
+        char [] guessChar = guess.toCharArray();
+        char [] list = listWords.toCharArray();
+        for (int i = 0; i < WORDLENGTH; i++)
+            for (int j = WORDLENGTH-1; j >= 0; j--)
+                if (guessChar[i] == list[j])
+                    matching++;
+        return matching;
     }
 
- }   
-    
+    private int countInPlace(String guess, String listWords) {
+        // returns # of in-place chars (helper of removeWords)
+        int inPlace = 0;
+        char [] guessChar = guess.toCharArray();
+        char [] list = listWords.toCharArray();
+        for(int i = 0; i < WORDLENGTH; i++)
+            if(guessChar[i] == list[i])
+                inPlace++;
+        return inPlace;
+    }
+
+    public void removeRepeatChars() { // removes words with repeated characters
+        for(int i = wordList.size()-1; i >= 0; i--) {
+            String word = wordList.get(i);
+            if(countMatching(word, word) > 5) // matching > 5 = repeated chars
+                wordList.remove(i);
+        }
+    }
+
+}
